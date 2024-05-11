373184931ca2411c6fa6aed1790ef73d8b90e202
RECodeVault <rlnvns@gmail.com>
2024-05-11 14:57:02 -0700

    Base template

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, jsonify
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.route("/fetch_data", methods = ["POST"])
+def fetch_data():
+    print("fetch_data")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
